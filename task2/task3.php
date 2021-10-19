08ad18bc978f423e40d644cd96c17b4b70688080
igor <i.tananakin@sprintf.ru>
2021-10-19 16:33:48 +0300

    substr

--- task2/task3.php
+++ task2/task3.php
@@ -1,14 +1,13 @@
 <?php
 
-function gender($firstName)
+function gander($firstName)
 {
     $notSex = ['Саша', 'Женя', 'Ли'];//массив одинаковых имён
     if (in_array($firstName, $notSex)) {
         return null;
 
     } else {
-        $count = strlen($firstName);
-        $symbol = substr($firstName, $count - 2);
+        $symbol = substr($firstName, - 2);
         switch ($symbol) {
             case 'я':
                 return 'ж';
@@ -23,15 +22,15 @@ function gender($firstName)
                 return null;
                 break;
         }
-        }
+    }
 
 }
-assert('ж' == gender('Аня'));
-assert('м' == gender('Игорь'));
-assert('м' == gender('Сергей'));
-assert('ж' == gender('Оля'));
-assert('ж' == gender('Мария'));
-assert('м' == gender('Олег'));
-assert(null === gender('y'));
-assert(null === gender(' '));
-assert(null == gender('Саша'));
+assert('ж' == gander('Аня'));
+assert('м' == gander('Игорь'));
+assert('м' == gander('Сергей'));
+assert('ж' == gander('Оля'));
+assert('ж' == gander('Мария'));
+assert('м' == gander('Олег'));
+assert(null === gander('y'));
+assert(null === gander(' '));
+assert(null == gander('Саша'));
