0c5e242feb69d8aeb6b677c97fc2858dba25f6bb
igor <i.tananakin@sprintf.ru>
2021-10-19 13:07:13 +0300

    с добавлением массива исключений

--- task2/task3.php
+++ task2/task3.php
@@ -2,8 +2,16 @@
 
 function name($firstName)
 {
+    $notSex = ['Саша', 'Женя', 'Ли'];//массив одинаковых имён
+    foreach ( $notSex as $value) {
+        if ($firstName == $value) {
+            return 'такое имя есть м ж';
+            break;
+        }
+    }
     $count = strlen($firstName);
     $symbol = substr($firstName, $count-2);
+
     switch($symbol) {
         case 'я':
             return 'ж';
@@ -20,13 +28,12 @@ function name($firstName)
     }
 }
 
-
 assert('ж' == name('Аня'));
 assert('м' == name('Игорь'));
 assert('м' == name('Сергей'));
 assert('ж' == name('Оля'));
-assert('м' == name('Саша'));
 assert('ж' == name('Мария'));
 assert('м' == name('Олег'));
-assert(null === name('Ан'));
+assert(null === name('y'));
 assert(null === name(' '));
+assert('такое имя есть м ж' == name('Саша'));
