09b17cefe0e856bb28697e8d328bdf2baf334ca1
igor <i.tananakin@sprintf.ru>
2021-10-22 14:54:16 +0300

    first commit

--- task4/upload.php
+++ task4/upload.php
@@ -2,7 +2,7 @@
 $types = ['image/png','image/jpeg','img/jpeg'];
 if (!empty($_FILES['myimage']) && in_array($_FILES['myimage']['type'], $types)) {
     if (0 == $_FILES['myimage']['error']) {
-        move_uploaded_file($_FILES['myimage']['tmp_name'], __DIR__.'/img/' . $_FILES['myimage']['name']);
+        move_uploaded_file($_FILES['myimage']['tmp_name'], __DIR__ . '/img/' . $_FILES['myimage']['name']);
     }
 
 } else {
