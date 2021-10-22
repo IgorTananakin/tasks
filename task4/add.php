48688322cd1e6a2513abbfdab255551c8f6e352c
igor <i.tananakin@sprintf.ru>
2021-10-22 15:07:58 +0300

    first commitss

--- task4/add.php
+++ task4/add.php
@@ -5,5 +5,5 @@ if (isset($_POST['record']) && !empty($_POST['record'])) {
     file_put_contents(__DIR__ . '/ghost.txt', implode("\n", $records));
 }
 
-header('Location: /task4/task1.php');
+header('Location: /task4/index1.php');
 exit;
